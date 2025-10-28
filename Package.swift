c9b2458fe1911a8a840931e7d61d034a7c479a1d
luo <luo_ty@qq.com>
2025-10-28 12:36:36 +0800

    fix swift-tools-version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.2
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "FFmpegKit",
     platforms: [
-        .iOS(.v12_1)
+        .iOS(.v12)
     ],
     products: [
         .library(
